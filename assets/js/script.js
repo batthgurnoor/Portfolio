256ed61b1b4454ea7121ab15b793f96facf9d89f
batthgurnoor <gurnoorbatth10@gmail.com>
2024-04-02 13:48:59 -0700

    About me section Completed and My resume Section started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,18 @@
-let navLinks = document.querySelectorAll("nav ul li a");
+let navLinks = document.querySelectorAll("a.inner-link");
 
 navLinks.forEach((item) => {
   item.addEventListener("click", function () {
+    console.log(item);
     document.querySelector("nav ul li a.active").classList.remove("active");
-    item.classList.add("active");
+    document.querySelector(`nav ul li a[href='${item.getAttribute("href")}']`).classList.add("active");
     document.querySelector("main >section.active").classList.remove("active");
-    document
-      .querySelector(`main>section${item.getAttribute("href")}`)
-      .classList.add("active");
+    document.querySelector(`main>section${item.getAttribute("href")}`).classList.add("active");
   });
 });
 
-document
-  .querySelector("#sidebar .toggle-sidebar")
-  .addEventListener("click", function () {
-    document.querySelector("#sidebar").classList.toggle("open");
-  });
+document.querySelector("#sidebar .toggle-sidebar").addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("open");
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   var atroposElement = document.querySelector(".my-atropos");
